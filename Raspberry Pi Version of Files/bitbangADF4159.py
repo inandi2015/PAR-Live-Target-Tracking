d06e1ef4b739c6db1bb6ed6815bc909e90a7f4fc
Iman Nandi <iman_nandi@yahoo.com>
2019-02-09 21:53:55 -0500

    Made synthesizer register driver

--- Raspberry Pi Version of Files/bitbangADF4159.py
+++ Raspberry Pi Version of Files/bitbangADF4159.py
@@ -6,9 +6,9 @@ GPIO.setmode(GPIO.BCM)
 
 global clock, le, txData, dataReg, muxOut
 clock = 17
-le = 27
+le = 18
 txData = 22
-dataReg = 18
+dataReg = 27
 muxOut = 23
 
 GPIO.setup(clock, GPIO.OUT)  # Set pin to output
@@ -20,7 +20,6 @@ GPIO.setup(txData, GPIO.OUT)  # Set pin to output
 GPIO.output(txData, False)  # Set pin to low ("False")
 GPIO.setup(dataReg, GPIO.OUT)  # Set pin to output
 GPIO.output(dataReg, False)  # Set pin to low ("False")
-
 
 def ADF4159Read(reg, data):
     d = list(bin(int(data, 16))[2:].zfill(4))
@@ -43,22 +42,20 @@ def ADF4159Read(reg, data):
     GPIO.output(clock, False)
 
     for i in range(len(bits)):
-        if bits[i] == 1:
-            time.sleep(0.000000025)
-            GPIO.output(clock, True)
-
-        time.sleep(0.000000025)
-        response[i] = GPIO.input(muxOut)
-
+	if bits[i] == 1:
+		time.sleep(0.000000025)
+		GPIO.output(clock, True)
+	time.sleep(0.000000025)
+	response[i] = GPIO.input(muxOut)
         if i == 3:
-            time.sleep(0.000000010)
-            GPIO.output(clock, False)
-            GPIO.output(le, False)
-            break
+		time.sleep(0.000000010)
+		GPIO.output(clock, False)
+		GPIO.output(le, False)
+		break
         else:
-            GPIO.output(clock, False)
-            time.sleep(0.000000000001)
-
+		GPIO.output(clock, False)
+		time.sleep(0.000000000001)
+        
     GPIO.output(le, False)
     print response
     return
@@ -66,60 +63,60 @@ def ADF4159Read(reg, data):
 
 def ADF4159Write(reg, data):
 	d = list(bin(int(data, 16))[2:].zfill(4))
-
+	 
 	for i in range(len(d)):
 		d[i] = int(d[i])
-
-	bits = [d[0], d[1], d[2], d[3]]
-	response = [0, 0, 0, 0, 0]
-
-    if bits[0] = 1:
-        GPIO.output(dataReg, True)
-    else:
-        GPIO.output(dataReg, False)
-
+	
+	bits = [d[0],d[1],d[2],d[3]]
+	response = [0,0,0,0,0]
+	
+	if bits[0] == 1:
+		GPIO.output(dataReg, True)
+	else:
+		GPIO.output(dataReg, False)
+		        
 	GPIO.output(clock, False)
-    GPIO.output(le, True)
-    time.sleep(0.00000000000001)
-
+	GPIO.output(le, True)
+	time.sleep(0.00000000000001)
+	
 	GPIO.output(le, False)
-    time.sleep(0.000000020) 
-    GPIO.output(clock, True)
-    time.sleep(0.000000025)
-    GPIO.output(clock, False)
+ 	time.sleep(0.000000020) 
+	GPIO.output(clock, True)
+	time.sleep(0.000000025)
+	GPIO.output(clock, False)
 
 	for i in range(len(bits)):
 		if bits[i] == 1:
-            GPIO.output(dataReg, True)
-            time.sleep(0.000000025)
+			GPIO.output(dataReg, True)
+			time.sleep(0.000000025)
 			GPIO.output(clock, True)
-
-        time.sleep(0.000000000001)
-        response[i] = GPIO.input(muxOut)
-
+        
+		time.sleep(0.000000000001)
+        	response[i] = GPIO.input(muxOut)
+	
 		if i == 3:
 			time.sleep(0.000000009999) # 10 ns including the response time before
-            GPIO.output(le, True)
-            time.sleep(0.000000015)
-            GPIO.output(clock, False)
-            time.sleep(0.00000005)
-            GPIO.output(le, False)
+           		GPIO.output(le, True)
+            		time.sleep(0.000000015)
+            		GPIO.output(clock, False)
+            		time.sleep(0.00000005)
+            		GPIO.output(le, False)
 			break 
-        else:
-            time.sleep(0.000000025)
-            GPIO.output(clock, False)
-            GPIO.output(dataReg, bits[i+1])
-            time.sleep(0.000000000001)
-		
+        	else:
+            		time.sleep(0.000000025)
+            		GPIO.output(clock, False)
+			GPIO.output(dataReg, bits[i+1])
+            		time.sleep(0.000000000001)
+	
 	GPIO.output(le, False)
 	print response
 	return
 
-while True:
-	ADF4159Read("A", "FF")  
-
 # while True:
-# 	ADF4159Write("A", "55")  
+# 	ADF4159Read("A", "F")  
+
+while True:
+ 	ADF4159Write("A", "5")  
 
 ADF4159Read("0", "9")	
 ADF4159Read("A", "5")
