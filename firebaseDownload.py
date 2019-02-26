896e4d15117e80c3befc425922948184658ef79a
Iman Nandi <iman_nandi@yahoo.com>
2019-02-26 03:27:22 -0500

    Modularized firebase upload and download and added handshaking

--- firebaseDownload.py
+++ firebaseDownload.py
@@ -6,13 +6,67 @@ import json
 import numpy
 from firebase import firebase 
 
-firebase = firebase.FirebaseApplication('https://par-live-target-tracking.firebaseio.com/DEV', None)
-result = firebase.get('/DEV', 'SignalData')
-print(result)
+### LOCAL MODULES ###
 
-dataOut = numpy.asarray(result['data']) # Convert result data from database into an array
-dataOut = dataOut.reshape(len(dataOut), 1) # Shape data for putting in one column of csv file
+def firebaseDownloadLocal(firebaseProject):
+    while True:
+        result = firebaseProject.get('/DEV', 'SignalData')
+        if result['Mode'] == 'Tracking' or result['Mode'] == 'Acquisition':
+            break
+        
+    print(result['Channel1'])
+    print(result['Channel2'])
+    print(result['Mode'])
+    print(result['PhaseDifference'])
+    
+    if result['Mode'] == 'Tracking':
+        dataOut1 = numpy.asarray(result['Channel1']) # Convert result data from database into an array
+        dataOut1 = dataOut1.reshape(len(dataOut1), 1) # Shape data for putting in one column of csv file
 
-with open(str(sys.argv[1]), "w") as f:
-    writer = csv.writer(f, dialect='excel', lineterminator = '\n')
-    writer.writerows(dataOut)
+        with open(str(sys.argv[1])+"1.csv", "w") as f:
+            writer = csv.writer(f, dialect='excel', lineterminator = '\n')
+            writer.writerows(dataOut1)
+
+        dataOut2 = numpy.asarray(result['Channel2']) # Convert result data from database into an array
+        dataOut2 = dataOut2.reshape(len(dataOut2), 1) # Shape data for putting in one column of csv file
+
+        with open(str(sys.argv[1])+"2.csv", "w") as f:
+            writer = csv.writer(f, dialect='excel', lineterminator = '\n')
+            writer.writerows(dataOut2)
+        
+        # Write to a CSV file here the tracking mode so MATLAB can check it
+        with open(str(sys.argv[1])+"Status.csv", "w") as f:
+            writer = csv.writer(f, dialect='excel', lineterminator = '\n')
+            writer.writerow([result['Mode']])
+    elif result['Mode'] == 'Acquisition':
+        # Write to a CSV file here the aquisition mode so MATLAB can check it
+        with open(str(sys.argv[1])+"Status.csv", "w") as f:
+            writer = csv.writer(f, dialect='excel', lineterminator = '\n')
+            writer.writerow([result['Mode']])
+
+        print("Acquisition Mode")
+
+### RADAR MODULES ###
+
+def firebaseDownloadRadar(firebaseProject):
+    while True:
+        result = firebaseProject.get('/DEV', 'SignalData')
+        if result['Mode'] == 'Start' or result['Mode'] == 'Stop' or result['Mode'] == 'Local':
+            break
+
+    print(result['Mode'])
+    print(result['PhaseDifference'])
+    
+    if result['Mode'] == 'Start':
+        print("Starting system...")
+        # Call driver stuff
+    elif result['Mode'] == 'Stop':  
+        print("Stopping system...")
+        # Kill everything
+    elif result['Mode'] == 'Local':
+        print("Steering beamformer...")
+        # Return phase difference result['PhaseDifference'] to steer beam
+        
+
+firebaseProject = firebase.FirebaseApplication('https://par-live-target-tracking.firebaseio.com/DEV', None)
+firebaseDownloadRadar(firebaseProject)
--- firebaseUpload.py
+++ firebaseUpload.py
@@ -6,12 +6,42 @@ import json
 import numpy
 from firebase import firebase 
 
-firebase = firebase.FirebaseApplication('https://par-live-target-tracking.firebaseio.com/DEV', None)
+### LoCAL MODULES ###
 
-with open(str(sys.argv[1]), newline='') as csvfile:
-    data = list(csv.reader(csvfile)) # Get list from csv file
+def firebaseUploadStart(firebaseProject):
+    dataJSON = {'Mode': 'Start', 'PhaseDifference': 'NA'}
+    firebaseProject.patch('/DEV/SignalData', dataJSON)
 
-dataArray = numpy.asarray(data).ravel().tolist() # Convert list to 2D array, unravel to 1D, and turn back to list
-dataJSON = {'data': dataArray} # Put data in firebase
+def firebaseUploadStop(firebaseProject):
+    dataJSON = {'Mode': 'Stop', 'PhaseDifference': 'NA'}
+    firebaseProject.patch('/DEV/SignalData', dataJSON)
 
-firebase.patch('/DEV/SignalData', dataJSON)
+def firebaseUploadLocal(firebaseProject):
+    dataJSON = {'Mode': 'Local', 'PhaseDifference': 10} # Insert parameter here in JSON format to also patch phase differnce calculation as an argument from MATLAB
+    firebaseProject.patch('/DEV/SignalData', dataJSON)
+
+### RADAR MODULES ###
+
+def firebaseUploadAcquisition(firebaseProject):
+    dataJSON = {'Mode': 'Acquisition'} # Insert parameter here in JSON format to also patch phase differnce calculation as an argument from MATLAB
+    firebaseProject.patch('/DEV/SignalData', dataJSON)
+
+def firebaseUploadTracking(firebaseProject):
+    with open(str(sys.argv[1])+".csv", newline='') as csvfile:
+        data1 = list(csv.reader(csvfile)) # Get list from csv file
+
+    dataArray1 = numpy.asarray(data1).ravel().tolist() # Convert list to 2D array, unravel to 1D, and turn back to list
+
+    with open(str(sys.argv[2])+".csv", newline='') as csvfile:
+        data2 = list(csv.reader(csvfile)) # Get list from csv file
+
+    dataArray2 = numpy.asarray(data2).ravel().tolist() # Convert list to 2D array, unravel to 1D, and turn back to list
+
+    dataJSON = {'Channel1': dataArray1, 'Channel2': dataArray2, 'Mode': 'Tracking',  'PhaseDifference': 'NA'} # Put data in firebase
+
+    firebaseProject.patch('/DEV/SignalData', dataJSON)
+
+
+firebaseProject = firebase.FirebaseApplication('https://par-live-target-tracking.firebaseio.com/DEV', None)
+# Execute function logic here or elsewhere as needed
+firebaseUploadLocal(firebaseProject)
