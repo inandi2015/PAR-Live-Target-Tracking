f1be80691039dad904ebf4c5408c6858db8923f6
Iman Nandi <iman_nandi@yahoo.com>
2019-04-29 01:33:12 -0400

    Fixed freeze of waveforms using timeout

--- Raspberry Pi Code/main.py
+++ Raspberry Pi Code/main.py
@@ -13,6 +13,7 @@ offsetAngle = 10 # Set how far to steer to left or right for tracking target
 beamformerPositions = 7 # Number of beamformerpositions (-60, -40, -20, 0, 20, 40, 60)
 degreesOfFreedom = 120 # Degrees of freedom for the beamformer steering
 angleCorrection = 60 # Set 120 degrees of freedom from 0 to 120 to be -60 to 60 with an offset of 60
+timeoutVal = 5 # Timeout for Waveforms in case it gets hung up
 
 waitStart = True
 while True: # Outer loop for keeping radar listening continuously
@@ -37,7 +38,12 @@ while True: # Outer loop for keeping radar listening continuously
                 position = positionNumber * angleDivision - angleCorrection
                 subprocess.call('python beamformerSteer.py ' + str(position), shell=True)
                 print("Acquiring amplitude...")
-                amplitude = subprocess.check_output(['python', 'AcquisitionIn.py']).decode('ascii')
+                amplitude = -1
+                while amplitude == -1:
+                    try:
+                        amplitude = subprocess.check_output(['python', 'AcquisitionIn.py'], timeout=timeoutVal).decode('ascii')
+                    except subprocess.TimeoutExpired:
+                        print("Timeout happened. Retrying...\n")
                 print("Done acquiring!")
 
                 if str(amplitude.splitlines()[-1]) != "failed to open device":
@@ -53,7 +59,14 @@ while True: # Outer loop for keeping radar listening continuously
                 angleDivision = int(degreesOfFreedom/int(beamformerPositions-1))
                 position = positionNumber * angleDivision - angleCorrection
                 subprocess.call('python beamformerSteer.py ' + str(position), shell=True)
-                amplitude = subprocess.check_output(['python', 'AcquisitionIn.py']).decode('ascii')
+
+                print("Measuring amplitude...")
+                amplitude = -1
+                while amplitude == -1:
+                    try:
+                        amplitude = subprocess.check_output(['python', 'AcquisitionIn.py'], timeout=timeoutVal).decode('ascii')
+                    except subprocess.TimeoutExpired:
+                        print("Timeout happened. Retrying...\n")
 
                 upload.firebaseUploadTracking(firebaseProject, int(position)) 
                 currentAngle = int(maxAmplitudeIndex) * int(180/int(beamformerPositions-1))
@@ -71,8 +84,14 @@ while True: # Outer loop for keeping radar listening continuously
                             print("Steer left!")
                             currentAngle = currentAngle - offsetAngle
                             subprocess.call('python beamformerSteer.py ' + str(currentAngle), shell=True)
-                        amplitude = subprocess.check_output(['python', 'AcquisitionIn.py']).decode('ascii')
+
                         print("Measuring amplitude...")
+                        amplitude = -1
+                        while amplitude == -1:
+                            try:
+                                amplitude = subprocess.check_output(['python', 'AcquisitionIn.py'], timeout=timeoutVal).decode('ascii')
+                            except subprocess.TimeoutExpired:
+                                print("Timeout happened. Retrying...\n")
 
                         if float(amplitude.splitlines()[-1]) > beamformerThreshold:
                             # state, phase = download.firebaseDownloadRadar(firebaseProject) 
